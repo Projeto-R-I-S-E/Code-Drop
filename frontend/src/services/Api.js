787652c66aaeba5d255bee949e3539520a9abdf6
Gustavo <159251895+GustavoAlbuquerquee@users.noreply.github.com>
2025-02-26 21:49:46 -0300

    Delete frontend directory

--- frontend/src/services/Api.js
+++
@@ -1,7 +0,0 @@
-const API_URL = 'http://localhost:5000/api';
-
-export const fetchHello = async () => {
-  const response = await fetch(`${API_URL}/hello`);
-  const data = await response.json();
-  return data;
-};
