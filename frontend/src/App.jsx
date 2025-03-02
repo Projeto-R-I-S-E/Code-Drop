b9cdba01e26dd40a509b02f30c9709818500f204
Matheus Mangualde <matheusmangualde@gmail.com>
2025-03-02 12:48:52 -0300

    feat: CopyButton funcionando

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { fetchHello, sendData } from "./services/Api";
+import { useState } from "react";
+import { sendData } from "./services/Api";
 import Modal from "./modal";
 
 function App() {
@@ -15,6 +15,23 @@ function App() {
     setLink(response.link); 
     setIsModalOpen(true);
   };
+
+  const CopyButton = () => {
+    const [copied, setCopied] = useState(false);
+    
+    const handleCopy = async () => {
+      const textToCopy = document.getElementById("linkText").innerText; // Pega o texto dentro do <a> com id "linkText"
+      
+      try {
+        await navigator.clipboard.writeText(textToCopy);
+        setCopied(true);
+  
+        // Limpar o estado após 2 segundos para reverter o texto do botão
+        setTimeout(() => setCopied(false), 2000);
+      } catch (err) {
+        console.error("Falha ao copiar:", err);
+      }
+    };
 
   return (
     <>
@@ -32,10 +49,12 @@ function App() {
           </form>
           <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
             <h2 className="text-xl font-bold">Compartilhar código</h2>
-            <p><a href={link}>{link}</a></p>
+            <p><a href={link} id="linktext">{link}</a></p>
             <br />
             <button onClick={() => setIsModalOpen(false)}>Fechar</button>
-            <button>Copiar link</button>
+            <button onClick={handleCopy}>
+            {copied ? "Copiado!" : "Copiar link"}
+            </button>
           </Modal>
           <div className="border-solid border-gray-200 w-full p-10">
             <h2 className="text-4xl font-bold text-blue-500 m-0">Listagem de links</h2>
