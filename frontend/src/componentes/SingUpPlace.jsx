01d2e94419aa2ff7e132718c32f1d05c8e52d5ac
Matheus Mangualde <matheusmangualde@gmail.com>
2025-03-05 11:50:44 -0300

    feat: atualizacao signupPlace

--- frontend/src/componentes/SingUpPlace.jsx
+++ frontend/src/componentes/SingUpPlace.jsx
@@ -18,39 +18,43 @@ const SignUpPlace = () => {
     }
 
     return (
-        <form 
-            onSubmit={handleSubmit} 
-            className="boreder-solid w-2/4 h-96 flex flex-col justify-center items-center gap-5"
-        >
-            <input 
-                type="text" 
-                name="nome" 
-                id="nome" 
-                required 
-                placeholder="Digite seu nome" 
-                value={nome} 
-                onChange={(e) => setNome(e.target.value)} 
-            />
-            <input 
-                type="email" 
-                name="email" 
-                id="email" 
-                required 
-                placeholder="Digite seu email" 
-                value={email} 
-                onChange={(e) => setEmail(e.target.value)} 
-            />
-            <input 
-                type="password" 
-                name="senha" 
-                id="senha" 
-                required 
-                placeholder="Digite sua senha" 
-                value={senha} 
-                onChange={(e) => setSenha(e.target.value)} 
-            />
-            <input type="submit" value="Enviar" className="cursor-pointer" />
-        </form>
+        <>
+            <div className='border-solid w-2/4 h-96 flex flex-col justify-center items-center gap-5'>
+                <form 
+                    onSubmit={handleSubmit} 
+                    className="w-2/4 h-96 flex flex-col justify-center items-center gap-5"
+                >
+                    <input 
+                        type="text" 
+                        name="nome" 
+                        id="nome" 
+                        required 
+                        placeholder="Digite seu nome" 
+                        value={nome} 
+                        onChange={(e) => setNome(e.target.value)} 
+                    />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        required 
+                        placeholder="Digite seu email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                    />
+                    <input 
+                        type="password" 
+                        name="senha" 
+                        id="senha" 
+                        required 
+                        placeholder="Digite sua senha" 
+                        value={senha} 
+                        onChange={(e) => setSenha(e.target.value)} 
+                    />
+                    <input type="submit" value="Enviar" className="cursor-pointer" />
+                </form>
+            </div>
+        </>
     );
 }
 
