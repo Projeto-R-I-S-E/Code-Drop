778dc49051915a0fcf85da433cba8bf1f997a09e
Matheus Mangualde <matheusmangualde@gmail.com>
2025-03-05 09:54:15 -0300

    feat: atualizando signuppage

--- frontend/src/SignUpPage.jsx
+++ frontend/src/SignUpPage.jsx
@@ -35,7 +35,7 @@ function SignUpPage() {
             <Header />
             <form onSubmit={handleSubmit} className="border-solid w-2/4 h-96 flex flex-col justify-center place-items-center gap-5" >
                 <input type="text" name="nome" id="nome" required placeholder="Digite seu nome" value={nome} onChange={(e) => setNome(e.target.value)} />
-                <input type="email" name="email" id="email" required placeholder="Digite seu email" value={email} />
+                <input type="email" name="email" id="email" required placeholder="Digite seu email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <input type="password" name="senha" id="senha" required placeholder="Digite sua senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
                 <input type="submit" value="Enviar" />
             </form>
