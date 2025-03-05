2b2ffebac73de2a9db49bd4b8001080f59c0dbc9
GustavoAlbuquerquee <gustavoalbuquerque807@gmail.com>
2025-03-05 12:19:45 -0300

    feat: enhance footer styling and update copyright text

--- frontend/src/componentes/footer.jsx
+++ frontend/src/componentes/footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-      <footer className="bg-[rgba(2,56,89,0.6)] text-white py-6 mt-10 w-full absolute bottom-0">
+      <footer className="bg-[rgba(2,56,89,0.9)] text-white py-6 mt-10 w-full absolute bottom-0 h-[136px]">
         <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
-          <p className="text-sm">&copy; {new Date().getFullYear()} Seu Site. Todos os direitos reservados.</p>
+          <p className="text-sm">&copy; {new Date().getFullYear()} Code-Drop Todos os direitos reservados.</p>
           <nav className="flex space-x-4 mt-4 md:mt-0">
             <a href="#" className="hover:underline">Sobre</a>
             <a href="#" className="hover:underline">Contato</a>
