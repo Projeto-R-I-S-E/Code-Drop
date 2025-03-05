d27a419ecbbe9c00b0eb26b980783ee38a4f9a47
Matheus Mangualde <matheusmangualde@gmail.com>
2025-03-05 11:40:49 -0300

    feat corrigindo tabela

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    nome = db.Column(db.String(150), nullable=False, unique=True)
+    nome = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     senha = db.Column(db.String(256), nullable=False)
 
