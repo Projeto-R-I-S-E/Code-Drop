7a4b8ac2b43813db22577d3a0c80bd4be1eb828a
GustavoAlbuquerquee <gustavoalbuquerque807@gmail.com>
2025-03-05 10:53:10 -0300

    feat: refactor SignUpPage to use SignUpPlace component for user registration

--- frontend/src/SignUpPage.jsx
+++ frontend/src/SignUpPage.jsx
@@ -1,31 +1,12 @@
-import { useState } from "react";
-import { registerUser } from "./services/Api"; 
 import Header from "./componentes/header";
+import SignUpPlace from "./componentes/SingUpPlace";
 
 function SignUpPage() {
-    const [nome, setNome] = useState("");
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-
-    async function handleSubmit(event) {
-        event.preventDefault();
-        const result = await registerUser(nome, email, senha);
-        if (result) {
-            alert("Usuário registrado com sucesso!");
-        } else {
-            alert("Erro ao registrar usuário.");
-        }
-    }
 
     return (
         <>
             <Header />
-            <form onSubmit={handleSubmit} className="border-solid w-2/4 h-96 flex flex-col justify-center place-items-center gap-5" >
-                <input type="text" name="nome" id="nome" required placeholder="Digite seu nome" value={nome} onChange={(e) => setNome(e.target.value)} />
-                <input type="email" name="email" id="email" required placeholder="Digite seu email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" name="senha" id="senha" required placeholder="Digite sua senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
-                <input type="submit" value="Enviar" />
-            </form>
+            <SignUpPlace />
         </>
     );
 }
---
+++ frontend/src/componentes/SingUpPlace.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { registerUser } from "../services/Api"; 
+
+const SignUpPlace = () => {
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const result = await registerUser(nome, email, senha);
+        if (result) {
+            alert("Usuário registrado com sucesso!");
+        } else {
+            alert("Erro ao registrar usuário.");
+        }
+    }
+
+    return (
+        <form 
+            onSubmit={handleSubmit} 
+            className="w-2/4 h-96 flex flex-col justify-center items-center gap-5"
+        >
+            <input 
+                type="text" 
+                name="nome" 
+                id="nome" 
+                required 
+                placeholder="Digite seu nome" 
+                value={nome} 
+                onChange={(e) => setNome(e.target.value)} 
+            />
+            <input 
+                type="email" 
+                name="email" 
+                id="email" 
+                required 
+                placeholder="Digite seu email" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+            />
+            <input 
+                type="password" 
+                name="senha" 
+                id="senha" 
+                required 
+                placeholder="Digite sua senha" 
+                value={senha} 
+                onChange={(e) => setSenha(e.target.value)} 
+            />
+            <input type="submit" value="Enviar" className="cursor-pointer" />
+        </form>
+    );
+}
+
+export default SignUpPlace;
