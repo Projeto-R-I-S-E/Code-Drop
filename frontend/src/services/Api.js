520e58070ac303e75c11d47f0e4efc7aed1ad167
Matheus Mangualde <matheusmangualde@gmail.com>
2025-03-04 21:15:33 -0300

    feat: teste 08

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,11 +1,5 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
-export const fetchHello = async () => {
-  const response = await fetch(`${API_URL}/hello`);
-  const data = await response.json();
-  return data;
-};
-
 export const sendData = async (text) => {
   try {
     const response = await fetch(`${API_URL}/submit`, {
