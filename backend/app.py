025e55d12626e94a3df27a50c2c09b1a280b3eb3
GustavoAlbuquerquee <gustavoalbuquerque807@gmail.com>
2025-03-06 16:29:39 -0300

    feat: remove debug print statement for new_link in submit function

--- backend/app.py
+++ backend/app.py
@@ -102,7 +102,6 @@ def submit():
             db.session.commit()	
         print(f'user returned: {user}')
         print(f'JWT Identity: {user_email}')
-        print(f'new_link: {new_link}')
 
         return jsonify({'link': link})
     except Exception as e:
