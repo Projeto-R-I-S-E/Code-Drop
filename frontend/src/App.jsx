e7898ee1cf907a25b71183ca6b8807898e470642
GustavoAlbuquerquee <gustavoalbuquerque807@gmail.com>
2025-03-07 18:28:35 -0300

    style: enhance layout and styling of App and Footer components for better user experience

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,17 +51,17 @@ function App() {
     <>
       <Header />
       <main className="m-10">
-        <div className="flex gap-48">
+        <div className="flex gap-48 h-4/5 ">
           {/* Formulário de envio */}
           <form onSubmit={handleSubmit} method="post">
             <textarea
               name="code"
               id="code"
               cols="70"
-              rows="15"
+              rows="16"
               value={text}
               onChange={(e) => setText(e.target.value)}
-              className="border-solid border-gray-300 text-2xl resize-none rounded-xl p-2"
+              className="border-solid border-gray-300 text-xl resize-none rounded-xl p-2"
               placeholder="Digite seu código"
               required
             ></textarea>
@@ -69,39 +69,41 @@ function App() {
             <input
               type="submit"
               value="Enviar código"
-              className="mt-4 p-2 bg-blue-500 text-white rounded"
+              className="bg-blue-800 text-white border border-white px-4 py-2 rounded-md mt-3"
             />
           </form>
 
           {/* Modal de compartilhamento */}
           <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-            <h2 className="text-xl font-bold">Compartilhar código</h2>
+            <h2 className="text-xl font-bold bg-white flex items-center justify-center">Compartilhar código</h2>
             <p>
-              <a href={link} id="linktext">
+              <a href={link} id="linktext" >
                 {link}
               </a>
             </p>
             <br />
-            <button onClick={() => setIsModalOpen(false)}>Fechar</button>
-            <button onClick={handleCopy}>
+            <div className="flex justify-center items-center gap-4">
+            <button onClick={() => setIsModalOpen(false)} className="bg-blue-800 text-white border border-white px-4 py-2 rounded-md">Fechar</button>
+            <button onClick={handleCopy} className="bg-blue-800 text-white border border-white px-4 py-2 rounded-md">
               {copied ? "Copiado!" : "Copiar Link"}
             </button>
+            </div>
           </Modal>
 
           {/* Listagem de Links */}
-          <div className="border-solid h-60 overflow-y-scroll border-gray-200 w-full p-10">
+          <div className="border-solid h-96 overflow-y-scroll border-gray-200 w-full p-10 rounded-xl">
             <h2 className="text-4xl font-bold text-blue-500 m-0">
               Listagem de links
             </h2>
             <hr />
             {userLinks.length > 0 ? (
               userLinks.map((linkItem) => (
-                <a key={linkItem.id} href={linkItem.url} className="text-2xl mt-5 block">
+                <a key={linkItem.id} href={linkItem.url} className="text-xl mt-5 block">
                   {linkItem.url}
                 </a>
               ))
             ) : (
-              <p className="text-gray-500">Nenhum link gerado ainda.</p>
+              <p className="text-gray-500 mt-5">Nenhum link gerado ainda.</p>
             )}
           </div>
         </div>
